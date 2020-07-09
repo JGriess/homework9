f9b589b28caafbba4676d7bb851a363c229b928e
JGries <jacksongriessen@gmail.com>
2020-07-09 01:54:40 -0500

    second commit

--- ReadME.md
+++ ReadME.md
@@ -1,16 +1,16 @@
 
-# f <svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='80' height='20'><linearGradient id='s' x2='0' y2='100%'><stop offset='0' stop-color='#bbb' stop-opacity='.1'/><stop offset='1' stop-opacity='.1'/></linearGradient><clipPath id='r'><rect width='80' height='20' rx='3' fill='#fff'/></clipPath><g clip-path='url(#r)'><rect width='63' height='20' fill='#555'/><rect x='63' width='17' height='20' fill='#007ec6'/><rect width='80' height='20' fill='url(#s)'/></g><g fill='#fff' text-anchor='middle' font-family='Verdana,Geneva,DejaVu Sans,sans-serif' text-rendering='geometricPrecision' font-size='110'><text x='325' y='150' fill='#010101' fill-opacity='.3' transform='scale(.1)' textLength='530'>License B</text><text x='325' y='140' transform='scale(.1)' textLength='530'>License B</text><text x='705' y='150' fill='#010101' fill-opacity='.3' transform='scale(.1)' textLength='70'>B</text><text x='705' y='140' transform='scale(.1)' textLength='70'>B</text></g></svg>
-## description
-f
+# hi <img src='https://img.shields.io/badge/LA-LicenseA-red'>
+## Description
+hello
 ## Table of Contents
 ### Installation
-f
+heyQ
 ### Usage
-f
+no!
 ### Credits
-f
+oh
 ### License
-lb
+la
 ## Questions
- Github: https://github.com/f
- Please reach me at fif you have any questions.
+ Github: https://github.com/JGries
+ Please reach me at JacksonGriessen@gmail.com if you have any questions.
--- index.js
+++ index.js
@@ -62,15 +62,15 @@ inquirer
   .then(function(response) {
     console.log(response.license);
       if(response.license == "la"){
-        badge = "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='80' height='20'><linearGradient id='s' x2='0' y2='100%'><stop offset='0' stop-color='#bbb' stop-opacity='.1'/><stop offset='1' stop-opacity='.1'/></linearGradient><clipPath id='r'><rect width='80' height='20' rx='3' fill='#fff'/></clipPath><g clip-path='url(#r)'><rect width='63' height='20' fill='#555'/><rect x='63' width='17' height='20' fill='#e05d44'/><rect width='80' height='20' fill='url(#s)'/></g><g fill='#fff' text-anchor='middle' font-family='Verdana,Geneva,DejaVu Sans,sans-serif' text-rendering='geometricPrecision' font-size='110'><text x='325' y='150' fill='#010101' fill-opacity='.3' transform='scale(.1)' textLength='530'>License A</text><text x='325' y='140' transform='scale(.1)' textLength='530'>License A</text><text x='705' y='150' fill='#010101' fill-opacity='.3' transform='scale(.1)' textLength='70'>A</text><text x='705' y='140' transform='scale(.1)' textLength='70'>A</text></g></svg>";
+        badge = "<img src='https://img.shields.io/badge/LA-LicenseA-red'>";
       }
       if(response.license == "lb"){
-        badge = "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='80' height='20'><linearGradient id='s' x2='0' y2='100%'><stop offset='0' stop-color='#bbb' stop-opacity='.1'/><stop offset='1' stop-opacity='.1'/></linearGradient><clipPath id='r'><rect width='80' height='20' rx='3' fill='#fff'/></clipPath><g clip-path='url(#r)'><rect width='63' height='20' fill='#555'/><rect x='63' width='17' height='20' fill='#007ec6'/><rect width='80' height='20' fill='url(#s)'/></g><g fill='#fff' text-anchor='middle' font-family='Verdana,Geneva,DejaVu Sans,sans-serif' text-rendering='geometricPrecision' font-size='110'><text x='325' y='150' fill='#010101' fill-opacity='.3' transform='scale(.1)' textLength='530'>License B</text><text x='325' y='140' transform='scale(.1)' textLength='530'>License B</text><text x='705' y='150' fill='#010101' fill-opacity='.3' transform='scale(.1)' textLength='70'>B</text><text x='705' y='140' transform='scale(.1)' textLength='70'>B</text></g></svg>";
+        badge = "<img src='https://img.shields.io/badge/LB-LicenseB-blue'>";
       }
       console.log(badge);
       var writeStream = fs.createWriteStream("ReadME.md");
       writeStream.write("\n# " + response.title + " " + badge )
-      writeStream.write("\n## description")
+      writeStream.write("\n## Description")
       writeStream.write("\n" + response.description);
       writeStream.write("\n## Table of Contents")
       writeStream.write("\n### Installation") 
@@ -83,7 +83,7 @@ inquirer
       writeStream.write("\n" + response.license);
       writeStream.write("\n## Questions")
       writeStream.write("\n Github: https://github.com/" + response.username)
-      writeStream.write("\n Please reach me at " + response.email + "if you have any questions.")
+      writeStream.write("\n Please reach me at " + response.email + " if you have any questions.")
       writeStream.end();
       
   });
